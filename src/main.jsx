fa2a7689561402e47d8f6963bff21e7cc4e97f2d
aishwarya <aishwaryaj1608@gmail.com>
2026-05-24 17:40:47 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './components/Error.jsx'
+import Login from './components/Login.jsx'
+
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element :<App></App>,
+    errorElement:<Error></Error>,
+    children:[
+      {
+        path:'/login',
+        element:<Login></Login>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter}></RouterProvider>
   </StrictMode>,
 )
